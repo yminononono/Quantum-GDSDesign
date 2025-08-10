7021dcda275a6c924a6b629aaf5c6df14634d4f8
Yuya Mino <yuya.mino@cern.ch>
2025-08-10 15:30:02 +0900

    Added option to set  resonator length by frequency

--- util/functions.py
+++ util/functions.py
@@ -130,6 +130,19 @@ def phidl_port_to_metal_pin(port):
 
     return point1, point2
 
+def calculate_effective_permittivity(w, s, h, eps_r):
+    k0 = w/(w + 2*s)
+    k0_prime = math.sqrt(1-pow(k0, 2))
+    k3 = math.tanh((math.pi*w)/(4*h))/math.tanh((math.pi*(w+2*s))/(4*h))
+    k3_prime = math.sqrt(1-pow(k3, 2))
+    K_k0 = ellipk(k0**2)
+    K_k0_prime=ellipk(k0_prime**2)
+    K_k3 = ellipk(k3**2)
+    K_k3_prime=ellipk(k3_prime**2)
+    K_tilde = (K_k0_prime/K_k0)*(K_k3/K_k3_prime)
+    eps_eff = (1 + eps_r * K_tilde)/(1 + K_tilde)
+    return eps_eff
+
 def calculate_resonator_frequency(
         length = 3000, # um
         core_width = 10, # um
@@ -152,19 +165,38 @@ def calculate_resonator_frequency(
     else:
         ValueError()
 
-    k0 = w/(w + 2*s)
-    k0_prime = math.sqrt(1-pow(k0, 2))
-    k3 = math.tanh((math.pi*w)/(4*h))/math.tanh((math.pi*(w+2*s))/(4*h))
-    k3_prime = math.sqrt(1-pow(k3, 2))
-    K_k0 = ellipk(k0**2)
-    K_k0_prime=ellipk(k0_prime**2)
-    K_k3 = ellipk(k3**2)
-    K_k3_prime=ellipk(k3_prime**2)
-    K_tilde = (K_k0_prime/K_k0)*(K_k3/K_k3_prime)
-    eps_eff = (1 + eps_r * K_tilde)/(1 + K_tilde)
+    eps_eff = calculate_effective_permittivity(w, s, h, eps_r)
     #eps_eff = 0.5*(1 + eps_r)
-    print("eps_eff : ", eps_eff)
     c_eff = c / math.sqrt(eps_eff)
     f = c_eff / (4*l)
 
-    return f
+    return f*1e-6
+
+def calculate_resonator_length(
+        frequency = 5000, # MHz
+        core_width = 10, # um
+        gap_width = 6, # um
+        height = 525, # um
+        material = "silicon"
+        ):
+
+    # convert um to m & MHz to Hz
+    f = frequency * 1e+6
+    w = core_width * 1e-6
+    s = gap_width * 1e-6
+    h = height * 1e-6
+
+    if material == "silicon":
+        eps_r = 11.9
+        #eps_r = 11.45
+    elif material == "sapphire":
+        eps_r = 9.4
+    else:
+        ValueError()
+
+    eps_eff = calculate_effective_permittivity(w, s, h, eps_r)
+    #eps_eff = 0.5*(1 + eps_r)
+    c_eff = c / math.sqrt(eps_eff)
+    l = c_eff / (4*f)
+
+    return l*1e+6
